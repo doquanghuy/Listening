0a1d114faf94b90dde58f21cf518797f2cfd0d19
doquanghuy <doquanghuy91@gmail.com>
2017-10-23 23:59:45 +0700

    add dropbox

--- Listening/AppDelegate.swift
+++ Listening/AppDelegate.swift
@@ -8,15 +8,15 @@
 
 import UIKit
 import CoreData
+import SwiftyDropbox
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        DropboxClientsManager.setupWithAppKey("db-2h7zkyrij961eob")
         return true
     }
 
